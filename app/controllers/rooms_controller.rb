128693d0b794735e01521de7dd619ba3b648ad0e
Amedes Júnior <amedes@gmail.com>
2023-05-26 21:27:59 -0300

    chap-app -> Mai, 26 de 2023 - Aula 29

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,58 @@
+class RoomsController < ApplicationController
+  before_action :set_room, only: %i[ show edit update destroy ]
+
+
+  def index
+    @rooms = Room.all
+  end
+
+
+  def show
+  end
+
+
+  def new
+    @room = Room.new
+  end
+
+
+  def edit
+  end
+
+  def create
+    @room = Room.new(room_params)
+    respond_to do |format|
+      if @room.save
+        format.turbo_stream {render turbo_stream: turbo_stream.append('rooms', partial:'shared/room', locals:{room: @room})}
+      else
+        format.turbo_stream {render turbo_stream: turbo_stream.replace('room_form', partial:'rooms/form', locals:{room:@room})}
+      end
+    end
+  end
+
+
+  def update
+    respond_to do |format|
+      if @room.update room_params
+        format.turbo_stream {render turbo_stream: turbo_stream.replace("room_#{@room.id}", partial:'shared/room', locals:{room: @room})}
+      else
+        format.html {render :edit}
+      #format.turbo_stream {render turbo_stream: turbo_stream.replace("room_#{@room.id}", partial:'rooms/form', locals:{room:@room})}
+      end
+    end
+  end
+
+
+  def destroy
+    @room.destroy
+  end
+
+  private
+    def set_room
+      @room = Room.find(params[:id])
+    end
+
+    def room_params
+      params.require(:room).permit(:name)
+    end
+end
